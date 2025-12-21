bbc859e38e669b5e008cace70a256de88555a327
eylul-patanoglu <medine.patanoglu@ogr.gelisim.edu.tr>
2025-12-21 20:03:48 +0300

    Update views.py

--- tarifler/views.py
+++ tarifler/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/ara")
+def ara():
+    sorgu = request.args.get("q")
+    return f"Aranan tarif: {sorgu}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 # Create your views here.
